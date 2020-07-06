190d4fb6ee161496fee27dd47a72973fae15a885
Faerryn <alexandre.liao@gmail.com>
2020-07-06 02:26:58 -0400

    Emacs theme

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,5 +1,17 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
+
 (add-to-list 'load-path "~/.emacs.d/packages/evil")
 (require 'evil)
 (evil-mode 1)
+
+(add-to-list 'load-path "~/.emacs.d/packages/dash.el")
+(require 'dash)
+
+(add-to-list 'load-path "~/.emacs.d/packages/autothemer")
+(require 'autothemer)
+
+(add-to-list 'load-path "~/.emacs.d/packages/emacs-theme-gruvbox")
+(require 'gruvbox)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/packages/emacs-theme-gruvbox")
+(load-theme 'gruvbox t)
