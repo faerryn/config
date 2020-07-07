be156d55f15cda78f0aee8e4fd7c16a8bb08f5eb
Faerryn <alexandre.liao@gmail.com>
2020-07-06 21:45:26 -0400

    Goto-chg

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,13 +1,13 @@
-; Clean UI
+;; Clean UI
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 
-; Load packages
+;; Load packages
 (let ((default-directory  "~/.emacs.d/lisp/"))
   (normal-top-level-add-subdirs-to-load-path))
 
-; Evil
+;; Evil
 (require 'evil)
 (evil-mode 1)
 
@@ -17,6 +17,6 @@
 (require 'evil-commentary)
 (evil-commentary-mode)
 
-; Aesthetics
+;; Aesthetics
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'gruvbox t)
