35de81c4687bf455620623a81e2afa76daf2171c
Faerryn <alexandre.liao@gmail.com>
2020-07-08 16:29:05 -0400

    Self-compiling, self-organizing, self-evolving super-AI!

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,10 +1,16 @@
-(let ((emacs_loadfile (expand-file-name "emacs" user-emacs-directory)))
-  (let ((emacs_org (concat emacs_loadfile ".org"))
-	(emacs_elc (concat emacs_loadfile ".elc")))
-    (if (file-newer-than-file-p emacs_org emacs_elc)
-      (progn
-	(setq load-prefer-newer t)
-	(let ((default-directory (expand-file-name "lisp" user-emacs-directory)))
-	  (normal-top-level-add-subdirs-to-load-path))
-	(org-babel-load-file emacs_org t))
-      (load emacs_loadfile))))
+;; Load packages
+(setq load-prefer-newer t)
+(let ((default-directory (expand-file-name "lisp" user-emacs-directory)))
+  (normal-top-level-add-subdirs-to-load-path))
+
+;; Load (and recompile) `emacs.org'
+(let ((emacs_loadfile (expand-file-name "emacs" user-emacs-directory))
+      (emacs_org (expand-file-name "emacs.org" user-emacs-directory)))
+  (if (file-newer-than-file-p emacs_org
+			      (emacs_elc (concat emacs_loadfile ".elc")))
+      (org-babel-load-file emacs_org t)
+    (load emacs_loadfile))))
+
+;; Recompile `init.el'
+(byte-recompile-file
+ (expand-file-name "init.el" user-emacs-directory) nil 0)
