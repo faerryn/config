1da58685e0064009d97cc5ed2b23cc4de4c81cbf
Faerryn <alexandre.liao@gmail.com>
2020-07-08 11:12:26 -0400

    Load packages in emacs.org

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,8 +1,3 @@
-;; Load packages
-(setq load-prefer-newer t)
-(let ((default-directory (expand-file-name "lisp" user-emacs-directory)))
-  (normal-top-level-add-subdirs-to-load-path))
-
 ;; Org
 (require 'org)
 (org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
