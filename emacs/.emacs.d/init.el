8c9ce152eba0e6732816334762b32f70375a581d
Faerryn <alexandre.liao@gmail.com>
2020-07-09 02:16:53 -0400

    emacs-lsp

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -5,6 +5,19 @@
        (expand-file-name "init.elc" user-emacs-directory))
       (byte-compile-file init_el t))
   (progn
+    ;; No frills
+    (menu-bar-mode -1)
+    (scroll-bar-mode -1)
+    (tool-bar-mode -1)
+    (tooltip-mode -1)
+    (setq inhibit-splash-screen t)
+    (setq auto-save-default nil)
+    (setq backup-inhibited t)
+    (setq create-lockfiles nil)
+    ;; Silent compilation warnings
+    (setq byte-compile-warnings nil)
+    (require 'warnings)
+    (setq display-warning-minimum-level :error)
     ;; Load packages
     (setq load-prefer-newer t)
     (let ((default-directory
