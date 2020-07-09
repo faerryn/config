eba8f8691959b03ff4a57a2ceda8eed6e423dc10
Faerryn <alexandre.liao@gmail.com>
2020-07-09 18:06:07 -0400

    Who knows

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zig-mode rust-mode evil-magit counsel ivy org-evil evil-textobj-column evil-exchange evil-matchit evil-commentary evil-surround evil-goggles evil-numbers evil-collection minions moody zenburn-theme use-package)))
+   '(evil-org zig-mode rust-mode evil-magit counsel ivy org-evil evil-textobj-column evil-exchange evil-matchit evil-commentary evil-surround evil-goggles evil-numbers evil-collection minions moody zenburn-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
