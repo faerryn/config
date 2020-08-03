e12b236020a0f1dad2fa58e215417a5b5b7db348
Faerryn <alexandre.liao@gmail.com>
2020-08-03 15:04:07 -0400

    Vis commentary

--- vis/.config/vis/visrc.lua
+++ vis/.config/vis/visrc.lua
@@ -1,10 +1,15 @@
 require('vis')
 
+require('vis-commentary/vis-commentary')
+
 vis.events.subscribe(vis.events.INIT, function()
-	-- Your global configuration options
+    -- Your global configuration options
+    vis:command('set tabwidth 4')
+    vis:command('set expandtab on')
+    vis:command('set autoindent on')
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	-- Your per window configuration options e.g.
-	-- vis:command('set number')
+    -- Your per window configuration options e.g.
+    vis:command('set relativenumbers on')
 end)
