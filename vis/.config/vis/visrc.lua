3fa0f41863ebdb3e05b6e0b9ed242d54d8f67f47
Faerryn <alexandre.liao@gmail.com>
2020-08-04 11:38:06 -0400

    Amazing powers

--- vis/.config/vis/visrc.lua
+++ vis/.config/vis/visrc.lua
@@ -4,12 +4,14 @@ require('vis-commentary/vis-commentary')
 
 vis.events.subscribe(vis.events.INIT, function()
     -- Your global configuration options
-    vis:command('set tabwidth 4')
-    vis:command('set expandtab on')
     vis:command('set autoindent on')
+    vis:command('set expandtab on')
+    vis:command('set tabwidth 4')
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
     -- Your per window configuration options e.g.
+    vis:command('set colorcolumn 80')
+    vis:command('set cursorline on')
     vis:command('set relativenumbers on')
 end)
