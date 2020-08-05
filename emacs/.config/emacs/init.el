e8852dd1cd0fb7cf739720552c0bc537505854b0
Faerryn <alexandre.liao@gmail.com>
2020-08-04 20:31:28 -0400

    Some things

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,17 +1,14 @@
 ;; No GC during init
 (setq gc-cons-threshold most-positive-fixnum)
-(add-hook 'after-init-hook (lambda () (setq gc-cons-threshold 800000)))
+(add-hook 'after-init-hook (lambda () (setq-default gc-cons-threshold 800000)))
 
 ;; Follow symlink (since tangled.el will be in a symlinked folder)
-(setq vc-follow-symlinks t)
+(setq-default vc-follow-symlinks t)
 
 ;; Tangle and load `init.org'
 (let ((init-org (expand-file-name "init.org" user-emacs-directory))
-      (tangled-el (expand-file-name "tangled.el" user-emacs-directory))
-      (tangled-elc (expand-file-name "tangled.elc" user-emacs-directory)))
+      (tangled-el (expand-file-name "tangled.el" user-emacs-directory)))
   (when (file-newer-than-file-p init-org tangled-el)
     (require 'ob-tangle)
     (org-babel-tangle-file init-org tangled-el))
-  (when (file-newer-than-file-p tangled-el tangled-elc)
-    (byte-compile-file tangled-el))
-  (load tangled-elc))
+  (load tangled-el))
