859f8c8ce1a54b9849d462f207efeb2b30addf9e
Faerryn <alexandre.liao@gmail.com>
2020-08-06 20:43:35 -0400

    Vis plugins??

--- vis/.config/vis/visrc.lua
+++ vis/.config/vis/visrc.lua
@@ -1,27 +1,25 @@
 require'vis'
 
-local vis_commentary = require'plugins/vis-commentary'
+require'vis-commentary'
 
-local vis_pairs = require'plugins/vis-pairs'
+local vis_pairs = require'vis-pairs'
 vis_pairs.autopairs = false
 
-local vis_surround = require'plugins/vis-surround'
+local vis_surround = require'vis-surround'
 vis_surround.prefix.add     = {"sa", "sa"}
 vis_surround.prefix.change  = {"sr", "sr"}
 vis_surround.prefix.delete  = {"sd", "sd"}
 
 vis.events.subscribe(vis.events.INIT, function()
     -- Your global configuration options
-
-    vis:command'set     autoindent          on'
-    vis:command'set     expandtab           on'
-    vis:command'set     tabwidth            4'
+    vis:command'set autoindent  on'
+    vis:command'set expandtab   on'
+    vis:command'set tabwidth    4'
     
     vis:command'map     normal  Q @@'
     vis:command'unmap   normal  s'
     vis:command'unmap   normal  S'
     vis:command'unmap   visual  s'
-
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
