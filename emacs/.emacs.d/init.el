a5f19b76681867abc16842c37626141b174f81c0
Faerryn <alexandre.liao@gmail.com>
2020-08-08 16:15:08 -0400

    Put everything in let

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,9 +1,7 @@
-(setq-default gc-cons-threshold most-positive-fixnum) ; No GC during init
-(add-hook 'after-init-hook (lambda () (setq-default gc-cons-threshold 800000)))
-(let (
-      (file-name-handler-alist nil) 	; Faster loading of files
-      (load-prefer-newer t)		; Load new .el over old .elc
-      (vc-follow-symlinks t)		; Follow stow's symlinks
+(let ((gc-cons-threshold most-positive-fixnum) ; No GC during init
+      (file-name-handler-alist nil)            ; Faster loading of files
+      (load-prefer-newer t)                    ; Load new .el over old .elc
+      (vc-follow-symlinks t)                   ; Follow stow's symlinks
       (init-org (expand-file-name "init.org" user-emacs-directory))
       (tangled-el (expand-file-name "tangled.el" user-emacs-directory)))
   (when (file-newer-than-file-p init-org tangled-el)
