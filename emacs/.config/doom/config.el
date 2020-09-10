9c6425b60976bc03c912b1c1f49be4f6279eca48
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-09-10 15:28:18 -0400

    Maximize Emacs on startup

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -2,7 +2,7 @@
 
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
-
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
