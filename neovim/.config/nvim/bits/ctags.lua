3f189889da989b44767144a3f951868efbfebd66
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-09-24 17:55:29 -0400

    More ctags should work good for things

--- neovim/.config/nvim/bits/ctags.lua
+++ neovim/.config/nvim/bits/ctags.lua
@@ -1,11 +1,6 @@
 function personal_ctags()
-	if not pcall(function() return vim.bo.tags end) or vim.bo.tags:len() == 0 then vim.bo.tags = vim.o.tags end
-	local status, path = pcall(function() return vim.bo.path end)
-	if not status or path:len() == 0 then path = vim.o.path end
-	vim.bo.tags = path:gsub('[^,]*,', function(capture)
-		if vim.fn.isdirectory(capture) then
-			return capture .. '/tags,'
-		end
-	end) .. vim.bo.tags
+	vim.bo.tags = vim.fn.eval'&path':gsub('[^,]*', function(capture)
+		if capture:len() > 0 then return capture .. '/tags,' end
+	end):gsub(',+', ','):gsub(',$', '') .. ',tags'
 end
 vim.cmd'autocmd BufEnter * lua personal_ctags()'
