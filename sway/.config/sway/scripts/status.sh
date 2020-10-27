843b7ec596a615b44b38f00aac900735f117ba12
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-10-27 15:20:38 -0400

    nice date and time on swaybar

--- sway/.config/sway/scripts/status.sh
+++ sway/.config/sway/scripts/status.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-module_date_time () {
-	date
-}
-
 module_charge () {
 	battery=/sys/class/power_supply/${1:-BAT1}
 	echo $(cat $battery/capacity)% $(cat $battery/status)
 }
 
 module_sound () {
-	echo $((100*$(aucatctl master | cut -d'=' -f2)/127))
+	echo VOL: $((100*$(aucatctl master | cut -d'=' -f2)/127))
 }
 
-echo "| vol: $(module_sound)% | $(module_date_time) | $(module_charge) |"
+echo "| $(module_sound)% | $(date +'%r') | $(date +'%a, %b %d %Y') | $(module_charge) |"
