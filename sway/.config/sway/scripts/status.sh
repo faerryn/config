1750eac3234bf70472445b188d1947098c5b891a
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-11-01 16:04:04 -0500

    le foo

--- sway/.config/sway/scripts/status.sh
+++ sway/.config/sway/scripts/status.sh
@@ -2,7 +2,7 @@
 
 module_charge () {
 	for battery in /sys/class/power_supply/BAT*; do
-		echo $(basename $battery): $(cat $battery/capacity)% $(cat $battery/status)
+		echo $(basename $battery): $(cat $battery/capacity)%
 	done
 }
 
@@ -10,4 +10,4 @@ module_sound () {
 	echo VOL: $((100*$(aucatctl master | cut -d'=' -f2)/127))
 }
 
-echo "| $(module_sound)% | $(date +'%r') | $(date +'%a, %b %d %Y') | $(module_charge) |"
+echo "$(module_sound)%\t$(date +'%r')\t$(date +'%a, %b %d, %Y')\t$(module_charge)"
