cdf9a2939dc815a4d9a86796eefde5966a16d8f5
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-11-02 11:15:34 -0500

    waybar

--- sway/.config/sway/scripts/status.sh
+++ sway/.config/sway/scripts/status.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
-module_charge () {
-	for battery in /sys/class/power_supply/BAT*; do
-		echo $(basename $battery): $(cat $battery/capacity)%
-	done
+volume_module() {
+	vol=$(aucatctl master | cut -d'=' -f2)
+	if test $vol -gt 96; then
+		echo 
+	elif test $vol -gt 32; then
+		echo 
+	elif test $vol -gt 0; then
+		echo 
+	else
+		echo 
+	fi
 }
 
-module_sound () {
-	echo VOL: $((100*$(aucatctl master | cut -d'=' -f2)/127))
+battery_module() {
+	bat=$(cat /sys/class/power_supply/BAT*/capacity)
+	if test $bat -gt 80; then
+		echo 
+	elif test $bat -gt 60; then
+		echo 
+	elif test $bat -gt 40; then
+		echo 
+	elif test $bat -gt 20; then
+		echo 
+	else
+		echo 
+	fi
 }
 
-echo "$(module_sound)%\t$(date +'%r')\t$(date +'%a, %b %d, %Y')\t$(module_charge)"
+echo "$(date +'%r, %D,') $(volume_module), $(battery_module)"
