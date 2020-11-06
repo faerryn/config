c376b1aa9380cc9cebfa76c36a017f88e02162c2
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-11-05 23:25:24 -0500

    le daemon

--- sway/.config/sway/scripts/daemon.sh
+++ sway/.config/sway/scripts/daemon.sh
@@ -4,5 +4,11 @@ if ! test -p ${XDG_RUNTIME_DIR}/wobpipe; then
 	mkfifo ${XDG_RUNTIME_DIR}/wobpipe
 fi
 
+pkill -x swayidle
+pkill -x tail
 pkill -x wob
+
+swayidle -w\
+	timeout 120 '$scripts/logout.sh lock'\
+	timeout 300 '$scripts/logout.sh suspend' &
 tail -f ${XDG_RUNTIME_DIR}/wobpipe | wob --background-color '#00000000' &
--- sway/.config/sway/scripts/idle.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-pkill -x swayidle
-swayidle -w\
-	timeout 120 '$scripts/logout.sh lock'\
-	timeout 300 '$scripts/logout.sh suspend' &
