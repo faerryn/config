df331c7e3313ac79ca2b201f82936e82cf7de443
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2020-11-27 00:47:09 -0500

    vis

--- vis/.config/vis/visrc.lua
+++ vis/.config/vis/visrc.lua
@@ -9,4 +9,5 @@ end)
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	-- Your per window configuration options e.g.
 	-- vis:command('set number')
+	vis:command('set relativenumbers')
 end)
