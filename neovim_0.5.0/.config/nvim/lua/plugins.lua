15047e3b5e9c274bbf26c0eaaccdb214956c79fa
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-02-11 02:25:44 -0500

    packer should install everything on first startup

--- neovim_0.5.0/.config/nvim/lua/plugins.lua
+++ neovim_0.5.0/.config/nvim/lua/plugins.lua
@@ -1,6 +1,12 @@
 local install_path = vim.fn.stdpath'data'..'/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) == 1 then
 	vim.fn.system('git clone --depth 1 https://github.com/wbthomason/packer.nvim.git '..install_path)
+	vim.api.nvim_exec([[
+	augroup personal_packer
+	autocmd!
+	autocmd VimEnter * PackerSync
+	augroup END
+	]], false)
 end
 
 require'packer'.startup{
