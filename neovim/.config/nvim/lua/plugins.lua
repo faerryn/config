46bf5b91fbed086080abb82ef3a867c4cf4cbf45
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-02-11 16:36:52 -0500

    gruvbox be back

--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -20,15 +20,27 @@ function use_packages(use)
 	use 'tpope/vim-eunuch'
 
 	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate',
-		config = function() require'nvim-treesitter.configs'.setup{highlights={enable=true}} end,
-	}
-
-	use {
 		'morhetz/gruvbox',
 		config = function()
+			vim.o.background = 'dark'
 			vim.o.termguicolors = true
+			vim.g.gruvbox_bold = 1
+			vim.g.gruvbox_italic = 1
+			vim.g.gruvbox_transparent_bg = 1
+			vim.g.gruvbox_underline = 1
+			vim.g.gruvbox_undercurl = 1
+			vim.g.gruvbox_termcolors = 256
+			vim.g.gruvbox_contrast_dark = 'medium'
+			vim.g.gruvbox_contrast_light = 'medium'
+			vim.g.gruvbox_italicize_comments = 1
+			vim.g.gruvbox_italicize_strings = 1
+			vim.g.gruvbox_invert_selection = 0
+			vim.g.gruvbox_invert_signs = 0
+			vim.g.gruvbox_invert_indent_guides = 0
+			vim.g.gruvbox_invert_tabline = 0
+			vim.g.gruvbox_improved_strings = 1
+			vim.g.gruvbox_improved_warnings = 1
+			vim.g.gruvbox_guisp_fallback = 1
 			vim.cmd [[colorscheme gruvbox]]
 		end,
 	}
@@ -183,6 +195,12 @@ if first_time then
 	packer.reset()
 	use_packages(packer.use)
 	packer.install()
+end
+
+if vim.fn.empty(vim.fn.glob(compile_path)) == 1 then
+	vim.cmd [[packadd packer.nvim]]
+	local packer = require'packer'
+
 	packer.compile()
 end
 
