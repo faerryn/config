f49791348df0fe7af909b2ae557fa4e9cc193243
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-02-17 13:35:07 -0500

    fugitive instead of neogit for now

--- neovim/.config/nvim/lua/modules/git.lua
+++ neovim/.config/nvim/lua/modules/git.lua
@@ -1,7 +1,11 @@
 return { setup = function()
 	-- neogit
-	vim.api.nvim_command'packadd neogit'
-	vim.api.nvim_set_keymap('n', '<Leader>g', [[<Cmd>lua require'neogit'.status.create'split'<CR>]], { noremap = true, silent = true })
+	-- vim.api.nvim_command'packadd neogit'
+	-- vim.api.nvim_set_keymap('n', '<Leader>g', [[<Cmd>lua require'neogit'.status.create'split'<CR>]], { noremap = true, silent = true })
+
+	-- neogit
+	vim.api.nvim_command'packadd vim-fugitive'
+	vim.api.nvim_set_keymap('n', '<Leader>g', '<Cmd>Git<CR>', { noremap = true, silent = true })
 
 	-- gitsigns.nvim
 	vim.api.nvim_command'packadd gitsigns.nvim'
