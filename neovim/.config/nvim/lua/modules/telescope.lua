f45c7526f3113b127ba22b7ef1e3f9a99317b8d1
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-03-03 11:42:11 -0500

    Revert "onehalfdark"
    This reverts commit cd204c1dc3b7dd169f1f7da8729c7f805dcdbb62.

--- neovim/.config/nvim/lua/modules/telescope.lua
+++ neovim/.config/nvim/lua/modules/telescope.lua
@@ -2,14 +2,11 @@ return { setup = function()
 	vim.api.nvim_command'packadd telescope.nvim'
 	vim.api.nvim_command'packadd plenary.nvim'
 	vim.api.nvim_command'packadd popup.nvim'
-	vim.api.nvim_command'packadd telescope-fzy-native.nvim'
 
 	require'telescope'.setup{ defaults = {
 		mappings = { i = { ['<C-w>c'] = require'telescope.actions'.close } },
 		file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
 	} }
-
-	require('telescope').load_extension('fzy_native')
 
 	local keymap_opts = { noremap = true, silent = true }
 	vim.fn.nvim_set_keymap('n', '<Leader>ff', [[<Cmd>lua require'telescope.builtin'.find_files{ hidden = true }<CR>]], keymap_opts)
