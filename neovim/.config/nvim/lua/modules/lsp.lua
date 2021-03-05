900dddcba8c72f53e282a9346525ee5b49e6c884
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-03-05 11:18:15 -0500

    lsp features

--- neovim/.config/nvim/lua/modules/lsp.lua
+++ neovim/.config/nvim/lua/modules/lsp.lua
@@ -4,7 +4,7 @@ return { setup = function()
 
 	local lspconfig = require'lspconfig'
 
-	local servers = { 'rust_analyzer' }
+	local servers = { 'clangd', 'rust_analyzer', 'zls' }
 
 	local on_attach = function(client, bufnr)
 		vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -12,10 +12,13 @@ return { setup = function()
 		vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', keymap_opts)
 		vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>',  keymap_opts)
 		vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K',  '<Cmd>lua vim.lsp.buf.hover()<CR>',       keymap_opts)
+
+		vim.api.nvim_command'command! -buffer Format lua vim.lsp.buf.formatting_sync()'
+		vim.api.nvim_command'command! -buffer Rename lua vim.lsp.buf.rename()'
+
 		require'completion'.on_attach(client, bufnr)
 	end
 
-	vim.api.nvim_command'command! LspRename lua vim.lsp.buf.rename()'
 
 	for _, server in ipairs(servers) do
 		lspconfig[server].setup{ on_attach = on_attach }
