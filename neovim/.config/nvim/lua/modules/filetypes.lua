ea52f24ffc3c63d42a77fce5a9899e044759129e
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-03-06 20:55:02 -0500

    filetypes Format

--- neovim/.config/nvim/lua/modules/filetypes.lua
+++ neovim/.config/nvim/lua/modules/filetypes.lua
@@ -3,4 +3,13 @@ return { setup = function()
 
 	vim.api.nvim_command'packadd zig.vim'
 	vim.g.zig_fmt_autosave = 0
+
+	vim.api.nvim_exec([[
+	augroup filetypes_format_command
+	autocmd!
+	autocmd FileType c,cpp command! Format %!clang-format
+	autocmd FileType rust command! Format %!rustfmt
+	autocmd FileType zig command! Format %!zig fmt --stdin
+	augroup END
+	]], false)
 end }
