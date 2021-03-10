1b91c434bd2688af274d4d74592f77dfc641ea54
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-03-09 23:18:35 -0500

    nlua.nvim for that lua goodness!

--- neovim/.config/nvim/lua/modules/filetypes.lua
+++ neovim/.config/nvim/lua/modules/filetypes.lua
@@ -3,13 +3,4 @@ return { setup = function()
 
 	vim.api.nvim_command'packadd zig.vim'
 	vim.g.zig_fmt_autosave = 0
-
-	vim.api.nvim_exec([[
-	augroup custom_filetypes
-	autocmd!
-	autocmd FileType c,cpp command! Format %!clang-format
-	autocmd FileType rust command! Format %!rustfmt
-	autocmd FileType zig command! Format %!zig fmt --stdin
-	augroup END
-	]], false)
 end }
--- neovim/.config/nvim/lua/modules/lsp.lua
+++ neovim/.config/nvim/lua/modules/lsp.lua
@@ -1,27 +1,32 @@
+local servers = { 'clangd', 'rust_analyzer', 'zls' }
+
+local on_attach = function(client, bufnr)
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	local keymap_opts = { noremap = true, silent = true }
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', keymap_opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>',  keymap_opts)
+	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K',  '<Cmd>lua vim.lsp.buf.hover()<CR>',       keymap_opts)
+
+	vim.api.nvim_command'command! -buffer CodeAction lua vim.lsp.buf.code_action()'
+	vim.api.nvim_command'command! -buffer Format lua vim.lsp.buf.formatting_sync()'
+	vim.api.nvim_command'command! -buffer -nargs=? Rename lua vim.lsp.buf.rename("<args>")'
+
+	require'completion'.on_attach(client, bufnr)
+end
+
 return { setup = function()
 	vim.api.nvim_command'packadd nvim-lspconfig'
 	vim.api.nvim_command'packadd completion-nvim'
 
 	local lspconfig = require'lspconfig'
 
-	local servers = { 'clangd', 'rust_analyzer', 'zls' }
-
-	local on_attach = function(client, bufnr)
-		vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-		local keymap_opts = { noremap = true, silent = true }
-		vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', keymap_opts)
-		vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>',  keymap_opts)
-		vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K',  '<Cmd>lua vim.lsp.buf.hover()<CR>',       keymap_opts)
-
-		vim.api.nvim_command'command! -buffer CodeAction lua vim.lsp.buf.code_action()'
-		vim.api.nvim_command'command! -buffer Format lua vim.lsp.buf.formatting_sync()'
-		vim.api.nvim_command'command! -buffer -nargs=? Rename lua vim.lsp.buf.rename("<args>")'
-
-		require'completion'.on_attach(client, bufnr)
-	end
-
-
 	for _, server in ipairs(servers) do
 		lspconfig[server].setup{ on_attach = on_attach }
 	end
+
+	vim.api.nvim_command'packadd nlua.nvim'
+	require('nlua.lsp.nvim').setup(require('lspconfig'), {
+		on_attach = on_attach,
+		globals = {},
+	} )
 end }
