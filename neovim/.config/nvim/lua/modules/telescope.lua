098bdb53f9b7e48f62be8d346b95334144ed1c02
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-03-09 22:38:47 -0500

    telescopic anger

--- neovim/.config/nvim/lua/modules/telescope.lua
+++ neovim/.config/nvim/lua/modules/telescope.lua
@@ -15,6 +15,5 @@ return { setup = function()
 	require('telescope').load_extension('fzy_native')
 
 	local keymap_opts = { noremap = true, silent = true }
-	vim.fn.nvim_set_keymap('n', '<Leader>ff', [[<Cmd>lua require'telescope.builtin'.find_files{ hidden = true }<CR>]], keymap_opts)
-	vim.fn.nvim_set_keymap('n', '<Leader>fl', [[<Cmd>lua require'telescope.builtin'.current_buffer_fuzzy_find()<CR>]], keymap_opts)
+	vim.api.nvim_set_keymap('n', '<Leader>f', [[<Cmd>lua require'telescope.builtin'.find_files{ hidden = true }<CR>]], keymap_opts)
 end }
