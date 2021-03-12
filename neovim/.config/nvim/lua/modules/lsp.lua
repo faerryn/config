95f39166b0c16302f649ff5cdb0565852393dd38
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-03-11 19:29:10 -0500

    make nlua work with my setup

--- neovim/.config/nvim/lua/modules/lsp.lua
+++ neovim/.config/nvim/lua/modules/lsp.lua
@@ -22,23 +22,11 @@ return { setup = function()
 		require'lspconfig'[server].setup{ on_attach = on_attach }
 	end
 
-	require'lspconfig'.sumneko_lua.setup{
-		on_attach = on_attach,
+	vim.api.nvim_command'packadd nlua.nvim'
+
+	require'nlua.lsp.nvim'.setup(require'lspconfig', {
 		cmd = { 'lua-language-server' },
-		settings = {
-			Lua = {
-				runtime = {
-					-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-					version = 'LuaJIT',
-					-- Setup your lua path
-					path = vim.split(package.path, ';'),
-				},
-				diagnostics = {
-					-- Get the language server to recognize the `vim` global
-					globals = {'vim'},
-				},
-			},
-		},
-	}
+		on_attach = on_attach,
+	})
 
 end }
