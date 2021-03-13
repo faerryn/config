bcb5e8262d1b16000ca0589d22bb7b0b97dc961f
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-03-13 11:08:42 -0500

    emacs config

---
+++ emacs/.config/emacs/early-init.el
@@ -0,0 +1,7 @@
+(let ((init_org (expand-file-name "init.org" user-emacs-directory))
+      (init_el (expand-file-name "init.el" user-emacs-directory)))
+  (if (file-newer-than-file-p init_org init_el)
+      (progn
+	(require 'org)
+	(require 'ob-tangle)
+	(org-babel-tangle-file init_org init_el "emacs-lisp"))))
