b0acc545c81c96df8fb4e04a2c0f9a4ee438cf6b
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-03-14 11:47:45 -0400

    lsp omnifunc re-added

--- neovim/.config/nvim/lua/modules/lsp.lua
+++ neovim/.config/nvim/lua/modules/lsp.lua
@@ -1,6 +1,8 @@
 local servers = { 'clangd', 'rust_analyzer', 'zls' }
 
 local on_attach = function(client, bufnr)
+	vim.bo.omnifunc = 'v:lua.vim.lsp.omnifunc'
+
 	local keymap_opts = { noremap = true, silent = true }
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', keymap_opts)
 	vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>',  keymap_opts)
@@ -9,6 +11,9 @@ local on_attach = function(client, bufnr)
 	vim.api.nvim_command'command! -buffer          CodeAction lua vim.lsp.buf.code_action()'
 	vim.api.nvim_command'command! -buffer          Format     lua vim.lsp.buf.formatting_sync()'
 	vim.api.nvim_command'command! -buffer -nargs=? Rename     lua vim.lsp.buf.rename("<args>")'
+
+	vim.api.nvim_command[[autocmd CursorHold  <buffer> lua vim.lsp.buf.signature_help()]]
+	vim.api.nvim_command[[autocmd CursorHoldI <buffer> lua vim.lsp.buf.signature_help()]]
 
 	vim.api.nvim_command'packadd completion-nvim'
 	require'completion'.on_attach()
