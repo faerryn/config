b6627d312e4fb57eb20a969850877bf1abf4dcac
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-03-14 12:00:47 -0400

    treesitter textobjects

--- neovim/.config/nvim/lua/modules/treesitter.lua
+++ neovim/.config/nvim/lua/modules/treesitter.lua
@@ -1,4 +1,38 @@
 return { setup = function()
 	vim.api.nvim_command'packadd nvim-treesitter'
-	require'nvim-treesitter.configs'.setup{ highlight = { enable = true } }
+	vim.api.nvim_command'packadd nvim-treesitter-textobjects'
+
+	require'nvim-treesitter.configs'.setup{
+		highlight = { enable = true },
+		textobjects = {
+			select = {
+				enable = true,
+				keymaps = {
+					['af'] = '@function.outer',
+					['if'] = '@function.inner',
+					['ac'] = '@class.outer',
+					['ic'] = '@class.inner',
+				},
+			},
+			move = {
+				enable = true,
+				goto_next_start = {
+					["]m"] = "@function.outer",
+					["]]"] = "@class.outer",
+				},
+				goto_next_end = {
+					["]M"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[m"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[M"] = "@function.outer",
+					["[]"] = "@class.outer",
+				},
+			},
+		},
+	}
 end }
