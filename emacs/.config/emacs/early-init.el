3a9dd8c8cd6b9a12cc5c13e020d4c2f8b7b8bd33
Jiang Meng Liao (Faerryn) <alexandre.liao@gmail.com>
2021-03-13 22:33:54 -0500

    byte-compile emacs init.org

--- emacs/.config/emacs/early-init.el
+++ emacs/.config/emacs/early-init.el
@@ -1,5 +1,6 @@
 ;; Disable Unwanted Features
 (setq-default
+ byte-compile-warnings nil
  make-backup-files nil
  package-enable-at-startup nil)
 (auto-save-mode -1)
@@ -14,8 +15,11 @@
 
 ;; Tangle init.org
 (let ((init.org (expand-file-name "init.org" user-emacs-directory))
-      (init.el (expand-file-name "init.el" user-emacs-directory)))
-  (if (file-newer-than-file-p init.org init.el)
-      (progn
-	(require 'ob-tangle)
-	(org-babel-tangle-file init.org init.el "emacs-lisp"))))
+      (init.el  (expand-file-name "init.el"  user-emacs-directory))
+      (init.elc (expand-file-name "init.elc" user-emacs-directory)))
+  (unless (file-newer-than-file-p init.el init.org)
+    (require 'ob-tangle)
+    (org-babel-tangle-file init.org init.el "emacs-lisp"))
+  (unless (file-newer-than-file-p init.elc init.el)
+    (require 'bytecomp)
+    (byte-compile-file init.el)))
